aae44fd1205096e9aa7035ac5d633f3d2cb05ac2
CovenantJunior <covenanttope@gmail.com>
2023-12-08 02:04:31 +0100

    Menu Scaffold

--- lib/layouts/menu.dart
+++ lib/layouts/menu.dart
@@ -5,6 +5,22 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        leading: const Icon(
+          Icons.menu,
+          color: Color.fromARGB(255, 138, 60, 55),
+        ),
+        title: const Text(
+          'Menus',
+          style: TextStyle(
+            color: Color.fromARGB(255, 138, 60, 55),
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[300],
+        ),
+    );
   }
 }
