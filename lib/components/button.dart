d7f5cb76e6d0dc98d1afe03fd41ef585dfe82aef
CovenantJunior <covenanttope@gmail.com>
2023-12-09 16:33:42 +0100

    Scrollable and better image appearance

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -11,31 +11,36 @@ class Button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(20.0),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(212, 135, 81, 77),
-          borderRadius: BorderRadius.circular(50)
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              text,
-              style: const TextStyle(
-                color: Colors.white
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(212, 135, 81, 77),
+            borderRadius: BorderRadius.circular(50)
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                text,
+                style: const TextStyle(
+                  color: Colors.white
+                ),
               ),
-            ),
-      
-            const SizedBox(width: 10.0),
-      
-            const Icon(
-              Icons.arrow_forward_rounded,
-              color: Colors.white,
-            )
-          ],
+        
+              const SizedBox(width: 10.0),
+        
+              const Icon(
+                Icons.arrow_forward_rounded,
+                color: Colors.white,
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/layouts/intro.dart
+++ lib/layouts/intro.dart
@@ -15,56 +15,58 @@ class _IntroState extends State<Intro> {
       backgroundColor: const Color.fromARGB(255, 138, 60, 55),
       body: Padding(
         padding: const EdgeInsets.all(25.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            const SizedBox(height: 20.0),
-
-            const Text(
-              "Sushi Restaurant",
-              style: TextStyle(
-                fontFamily: 'DM Serif Display',
-                fontSize: 40.0,
-                color: Colors.white
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const SizedBox(height: 20.0),
+          
+              const Text(
+                "Sushi Restaurant",
+                style: TextStyle(
+                  fontFamily: 'DM Serif Display',
+                  fontSize: 40.0,
+                  color: Colors.white
+                )
+              ),
+          
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Image.asset('assets/images/salmon_eggs.png'),
+              ),
+          
+              const SizedBox(height: 30.0),
+          
+               const Text(
+                "THE FLAVORS OF JAPANESE CUISINE.",
+                style: TextStyle(
+                  fontFamily: 'DM Serif Display',
+                  fontSize: 30.0,
+                  color: Colors.white
+                )
+              ),
+          
+              const SizedBox(height: 10.0),
+          
+               Text(
+                "Experience the tasty essence of popular Japanese cuisine that's available worldwide, making every bite a flavorful journey without limits.",
+                style: TextStyle(
+                  color: Colors.grey[200]
+                )
+              ),
+          
+          
+              const SizedBox(height: 40.0),
+          
+              Button(
+                text: "Get Started",
+                onTap: () {
+                  Navigator.pushNamed(context, '/menu');
+                },
               )
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Image.asset('assets/images/salmon_eggs.png'),
-            ),
-
-            const SizedBox(height: 30.0),
-
-             const Text(
-              "THE FLAVORS OF JAPANESE CUISINE.",
-              style: TextStyle(
-                fontFamily: 'DM Serif Display',
-                fontSize: 30.0,
-                color: Colors.white
-              )
-            ),
-
-            const SizedBox(height: 10.0),
-
-             Text(
-              "Experience the tasty essence of popular Japanese cuisine that's available worldwide, making every bite a flavorful journey without limits.",
-              style: TextStyle(
-                color: Colors.grey[200]
-              )
-            ),
-
-
-            const SizedBox(height: 40.0),
-
-            Button(
-              text: "Get Started",
-              onTap: () {
-                Navigator.pushNamed(context, '/menu');
-              },
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
