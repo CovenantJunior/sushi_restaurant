ee9ff1662e4063280ce1517a9a72afee76a696ae
CovenantJunior <covenanttope@gmail.com>
2023-12-14 02:17:19 +0100

    Tap gesture

--- lib/components/food_tile.dart
+++ lib/components/food_tile.dart
@@ -3,71 +3,75 @@ import 'package:sushi_restaurant/models/menu_list.dart';
 // import 'package:sushi_restaurant/themes/colors.dart';
 
 class FoodTile extends StatelessWidget {
-  
   final MenuList food;
+  final void Function()? onTap;
   const FoodTile({
     super.key,
-    required this.food
+    required this.food,
+    required this.onTap
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(right: 20.0),
-      padding: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        color: Colors.grey[100],
-        borderRadius: BorderRadius.circular(20)
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset(
-            food.imagePath,
-            height: 150,
-          ),
-            
-          Text(
-            food.name,
-            style: const TextStyle(
-              fontFamily: 'DM Serif Display',
-              fontSize: 20
+    return GestureDetector(
+      onTap: null,
+      child: Container(
+        margin: const EdgeInsets.only(right: 20.0),
+        padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: Colors.grey[100],
+          borderRadius: BorderRadius.circular(20)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              food.imagePath,
+              height: 150,
             ),
-          ),
-            
-          SizedBox(
-            width: 160.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  food.price,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-            
-                const SizedBox(height: 20.0),
-            
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      color: Colors.yellow[700],
+              
+            Text(
+              food.name,
+              style: const TextStyle(
+                fontFamily: 'DM Serif Display',
+                fontSize: 20
+              ),
+            ),
+              
+            SizedBox(
+              width: 160.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    food.price,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold
                     ),
-                    Text(
-                      food.rating,
-                      style: const TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold
+                  ),
+              
+                  const SizedBox(height: 20.0),
+              
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow[700],
                       ),
-                    )
-                  ],
-                ),
-              ],
+                      Text(
+                        food.rating,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
