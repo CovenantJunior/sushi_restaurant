7fbe982fc9b9b9d878ed85d28842066e8801c252
CovenantJunior <covenanttope@gmail.com>
2023-12-15 05:56:17 +0100

    SafeArea() and Align to End

--- lib/layouts/intro.dart
+++ lib/layouts/intro.dart
@@ -13,49 +13,51 @@ class _IntroState extends State<Intro> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 138, 60, 55),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(25.0,25.0,25.0,0.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const SizedBox(height: 20.0),
-          
-              const Text(
-                "Sushi Restaurant",
-                style: TextStyle(
-                  fontFamily: 'DM Serif Display',
-                  fontSize: 40.0,
-                  color: Colors.white
-                )
-              ),
-          
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Image.asset('images/salmon_eggs.png'),
-              ),
-          
-              const SizedBox(height: 30.0),
-          
-               const Text(
-                "THE FLAVORS OF JAPANESE CUISINE.",
-                style: TextStyle(
-                  fontFamily: 'DM Serif Display',
-                  fontSize: 30.0,
-                  color: Colors.white
-                )
-              ),
-          
-              const SizedBox(height: 10.0),
-          
-               Text(
-                "Experience the tasty essence of popular Japanese cuisine that's available worldwide, making every bite a flavorful journey without limits.",
-                style: TextStyle(
-                  color: Colors.grey[200]
-                )
-              ),
-            ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(25.0,25.0,25.0,0.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const SizedBox(height: 20.0),
+            
+                const Text(
+                  "Sushi Restaurant",
+                  style: TextStyle(
+                    fontFamily: 'DM Serif Display',
+                    fontSize: 40.0,
+                    color: Colors.white
+                  )
+                ),
+            
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Image.asset('images/salmon_eggs.png'),
+                ),
+            
+                const SizedBox(height: 30.0),
+            
+                 const Text(
+                  "THE FLAVORS OF JAPANESE CUISINE.",
+                  style: TextStyle(
+                    fontFamily: 'DM Serif Display',
+                    fontSize: 30.0,
+                    color: Colors.white
+                  )
+                ),
+            
+                const SizedBox(height: 10.0),
+            
+                 Text(
+                  "Experience the tasty essence of popular Japanese cuisine that's available worldwide, making every bite a flavorful journey without limits.",
+                  style: TextStyle(
+                    color: Colors.grey[200]
+                  )
+                ),
+              ],
+            ),
           ),
         ),
       ),
