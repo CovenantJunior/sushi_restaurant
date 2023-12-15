d8ab6f5740c44d439ffa8e4439bdb1589952f1e9
CovenantJunior <covenanttope@gmail.com>
2023-12-15 15:06:19 +0100

    Add to cart UI and increment

---
+++ lib/components/add_to_cart.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:sushi_restaurant/themes/colors.dart';
+
+class AddToCart extends StatefulWidget {
+  
+
+  @override
+  State<AddToCart> createState() => _AddToCartState();
+  final String price;
+  const AddToCart({
+    super.key,
+    required this.price
+  });
+}
+
+class _AddToCartState extends State<AddToCart> {
+  int amount = 0;
+
+  void increment() {
+    amount++;
+    setState(() {
+      amount = amount;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: null,
+      child: Container(
+        height: 70,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: primaryColor,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  widget.price,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16
+                  ),
+                ),
+
+                const SizedBox(width: 10),
+
+                 Container(
+                  decoration: BoxDecoration(
+                    color: secondaryColor,
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                   child: IconButton(
+                    onPressed: increment,
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    )
+                  ),
+                 ),
+
+                 const SizedBox(width: 10),
+
+                 Text(
+                  "$amount",
+                  style: const TextStyle(
+                    color: Colors.white
+                  ),
+                 ),
+
+                const SizedBox(width: 10),
+                
+                Container(
+                  decoration: BoxDecoration(
+                    color: secondaryColor,
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: const IconButton(
+                    onPressed: null,
+                    icon: Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                    )
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/layouts/food_details.dart
+++ lib/layouts/food_details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sushi_restaurant/components/add_to_cart.dart';
 import 'package:sushi_restaurant/models/menu_list.dart';
+import 'package:sushi_restaurant/themes/colors.dart';
 
 class FoodDetails extends StatelessWidget {
   
@@ -13,7 +15,94 @@ class FoodDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(food.name),
+      appBar: (
+        AppBar(
+          foregroundColor: primaryColor,
+          title: Text(
+            food.name,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: "DM Serif Display"
+            ),
+          ),
+          centerTitle: true,
+        )
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      food.imagePath,
+                      height: 200,
+                    ),
+                
+                    const SizedBox(height: 20),
+                
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellow[700],
+                        ),
+                
+                        const SizedBox(width: 5),
+                        
+                        Text(
+                          food.rating,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ],
+                    ),
+                    
+                    const SizedBox(height: 25),
+                
+                    Text(
+                      food.name,
+                      style: TextStyle(
+                        fontFamily: 'DM Serif Display',
+                        color: primaryColor,
+                        fontSize: 30
+                      ),
+                    ),
+                
+                    const SizedBox(height: 20),
+                
+                    Text(
+                      "Description",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: primaryColor,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                
+                    const SizedBox(height: 20),
+                
+                    Text(
+                      "Indulge in the ocean's finest treasure with our Salmon Eggs Delight, featuring premium-grade ikura harvested from the freshest salmon. These glistening, vibrant orbs offer a burst of oceanic flavors and a delightful texture that tantalizes the palate.\r\n\r\nSavor the exquisite brininess and subtle sweetness of our hand-picked salmon roe, encapsulated within delicate clusters. Each pop reveals a luscious, velvety texture and a satisfying burst of umami, elevating your culinary experience.",
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: AddToCart(price: food.price),
     );
   }
 }
